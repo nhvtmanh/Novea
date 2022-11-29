30f29b6ad9315c8f575f50f16bdf691f44d2d00a
Vũ Đức Minh <21522348@ms.uit.edu.vn>
2022-11-29 22:42:55 +0700

    doi giao dien

--- Novea/ViewModel/BaseViewModel.cs
+++ Novea/ViewModel/BaseViewModel.cs
@@ -5,8 +5,9 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
-namespace ClothesShopManagement.ViewModel
+namespace Novea.ViewModel
 {
     public class BaseViewModel : INotifyPropertyChanged
     {
@@ -17,6 +18,43 @@ namespace ClothesShopManagement.ViewModel
             PropertyChangedEventHandler handler = PropertyChanged;
             if (handler != null)
                 handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Predicate<T> _canExecute;
+        private readonly Action<T> _execute;
+
+        public RelayCommand(Predicate<T> canExecute, Action<T> execute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+            _canExecute = canExecute;
+            _execute = execute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            try
+            {
+                return _canExecute == null ? true : _canExecute((T)parameter);
+            }
+            catch
+            {
+                return true;
+            }
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute((T)parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
         }
     }
 }
