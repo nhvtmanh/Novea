85c3802ff071769338865830716446eac677490c
Vũ Đức Minh <21522348@ms.uit.edu.vn>
2022-12-08 17:05:13 +0700

    setting

--- Novea/View/Admin/PersonInfoSetting.xaml.cs
+++ Novea/View/Admin/PersonInfoSetting.xaml.cs
@@ -10,16 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Novea.View.Login
+namespace Novea.View.Admin
 {
     /// <summary>
-    /// Interaction logic for SignUp.xaml
+    /// Interaction logic for PersonInfoSetting.xaml
     /// </summary>
-    public partial class SignUp : Window
+    public partial class PersonInfoSetting : Page
     {
-        public SignUp()
+        public PersonInfoSetting()
         {
             InitializeComponent();
         }
