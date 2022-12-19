578f0905c7717e4b3776d51839645dc9d303a357
nhvtmanh <21522329@gm.uit.edu.vn>
2022-12-19 21:41:52 +0700

    add storeviewmodel and display list of stores

---
+++ Novea/ViewModel/Client/StoreViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Novea.Model;
+using Novea.View.Admin;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows.Input;
+
+namespace Novea.ViewModel.Client
+{
+    public class StoreViewModel : BaseViewModel
+    {
+        private ObservableCollection<CUAHANG> _listStore;
+        public ObservableCollection<CUAHANG> listStore
+        {
+            get => _listStore;
+            set { _listStore = value; }
+        }
+        private ObservableCollection<CUAHANG> _listStore1;
+        public ObservableCollection<CUAHANG> listStore1
+        {
+            get => _listStore1;
+            set { _listStore1 = value; }
+        }
+        public ICommand LoadStoreCommand { get; set; }
+        public StoreViewModel()
+        {
+            listStore1 = new ObservableCollection<CUAHANG>(DataProvider.Ins.DB.CUAHANGs);
+            listStore = new ObservableCollection<CUAHANG>(listStore1.GroupBy(p => p.TENCH).Select(grp => grp.FirstOrDefault()));
+            LoadStoreCommand = new RelayCommand<HomeView>((p) => true, (p) => loadStore(p));
+        }
+        void loadStore(HomeView parameter)
+        {
+            listStore1 = new ObservableCollection<CUAHANG>(DataProvider.Ins.DB.CUAHANGs);
+            listStore = new ObservableCollection<CUAHANG>(listStore1.GroupBy(p => p.TENCH).Select(grp => grp.FirstOrDefault()));
+        }
+    }
+}
