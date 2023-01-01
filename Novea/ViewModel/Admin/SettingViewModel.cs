d4952c236c7b389f5ae4a99236132915f7d8efc4
Vũ Đức Minh <21522348@ms.uit.edu.vn>
2023-01-01 23:59:29 +0700

    update professional setting view

---
+++ Novea/ViewModel/Admin/SettingViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Novea.Model;
+using Novea.View;
+using Novea.View.Admin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Xml.Linq;
+
+
+namespace Novea.ViewModel.Admin
+{
+    public class SettingViewModel : BaseViewModel
+    {
+
+    }
+}
