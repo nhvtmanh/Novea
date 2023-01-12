aabebae29861932fe3bddd9955020907b4ebd3a1
Minh Nguyễn Hải <21522342@ms.uit.edu.vn>
2023-01-12 16:17:25 +0700

    push

--- Novea/ViewModel/Admin/HomeViewModel.cs
+++ Novea/ViewModel/Admin/HomeViewModel.cs
@@ -1,8 +1,17 @@
-﻿using System;
+﻿using Novea.Model;
+using Novea.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using LiveCharts;
+using LiveCharts.Wpf;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.ObjectModel;
+using LiveCharts.Defaults;
 
 namespace Novea.ViewModel.Admin
 {
---
+++ Novea/ViewModel/Admin/OrdersViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Novea.Model;
+using Novea.View;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Novea.ViewModel.Admin
+{
+    public class OrdersViewModel : BaseViewModel
+    {
+
+    }
+}
