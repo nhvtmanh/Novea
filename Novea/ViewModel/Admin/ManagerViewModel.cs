252755250f4f1942fb94a40565c0440ca3920736
Vũ Đức Minh <21522348@ms.uit.edu.vn>
2023-01-18 22:52:20 +0700

    Revert "finish manager view model"
    This reverts commit a824ea9e982f64038019feb419b70af344af69dd.

--- Novea/ViewModel/Admin/ManagerViewModel.cs
+++ Novea/ViewModel/Admin/ManagerViewModel.cs
@@ -1,10 +1,17 @@
-﻿using Novea.Model;
+﻿using MaterialDesignThemes.Wpf;
+using Novea.Model;
+using Novea.View;
 using Novea.View.Admin;
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media.Imaging;
 
@@ -19,27 +26,28 @@ namespace Novea.ViewModel.Admin
         public ObservableCollection<HOADON> listHD1 { get => _listHD1; set { _listHD1 = value; OnPropertyChanged(); } }
         public ICommand SearchCommand { get; set; }
         public ICommand DetailPdCommand { get; set; }
+        public ICommand AddPdPdCommand { get; set; }
         public ICommand LoadCsCommand { get; set; }
-        public ICommand SortCommand { get; set; }
+        private ObservableCollection<string> _listTK;
+        public ObservableCollection<string> listTK { get => _listTK; set { _listTK = value; OnPropertyChanged(); } }
+        public ICommand Filter { get; set; }
 
         public ManagerViewModel()
         {
             listHD1 = new ObservableCollection<HOADON>(DataProvider.Ins.DB.HOADONs);
             SearchCommand = new RelayCommand<ManagerView>((p) => { return p == null ? false : true; }, (p) => _SearchCommand(p));
-            SortCommand = new RelayCommand<ManagerView>((p) => { return p == null ? false : true; }, (p) => _SortCommand(p));
-            DetailPdCommand = new RelayCommand<ManagerView>((p) => { return p.ListViewBill.SelectedItem == null ? false : true; }, (p) => _DetailPd(p));
             LoadCsCommand = new RelayCommand<ManagerView>((p) => true, (p) => _LoadCsCommand(p));
             listHD = new ObservableCollection<HOADON>(listHD1.GroupBy(p => p.SOHD).Select(grp => grp.FirstOrDefault()));
-
             if (DesignerProperties.GetIsInDesignMode(new DependencyObject()))
-                return;           
+                return;
         }
         void _LoadCsCommand(ManagerView parameter)
         {
+            listTK = new ObservableCollection<string>() { "Tên SP", "Giá SP" };
             listHD = new ObservableCollection<HOADON>(DataProvider.Ins.DB.HOADONs);
             listHD = new ObservableCollection<HOADON>(listHD1.GroupBy(p => p.SOHD).Select(grp => grp.FirstOrDefault()));
-            parameter.cbxChon.SelectedIndex = 0;
-            parameter.ListViewBill.Items.SortDescriptions.Add(new SortDescription("SOHD", ListSortDirection.Ascending));
+            _Filter(parameter);
+            //_SearchCommand(parameter);
         }
         void _SearchCommand(ManagerView paramater)
         {
@@ -68,22 +76,35 @@ namespace Novea.ViewModel.Admin
             }
         }
 
-        void _SortCommand(ManagerView parameter)
+        void _Filter(ManagerView parameter)
         {
-            var SortDirection = parameter.cbxChon.SelectedIndex.ToString() == "0" ? ListSortDirection.Ascending : ListSortDirection.Descending;
-            parameter.ListViewBill.Items.SortDescriptions[0] = new SortDescription("SOHD", SortDirection);
+            var SortProperty = parameter.cbxChon.Text;
+            
+
+            switch (parameter.cbxChon.SelectedIndex.ToString())
+            {
+                case "0":
+                    {
+                        listHD = new ObservableCollection<HOADON>(listHD1.GroupBy(p => p.SOHD).Select(grp => grp.FirstOrDefault()));
+                        parameter.ListViewBill.ItemsSource = listHD;
+                        break;
+                    }
+                case "1":
+                    {
+                        listHD = new ObservableCollection<HOADON>(listHD1.GroupBy(p => p.SOHD).Select(grp => grp.FirstOrDefault())/*.Where(p => p.SOHD)*/);
+                        parameter.ListViewBill.ItemsSource = listHD;
+                        break;
+                    }
+                case "2":
+                    {
+                        listHD = new ObservableCollection<HOADON>(listHD1.GroupBy(p => p.SOHD).Select(grp => grp.FirstOrDefault())/*.Where(p => p.SOHD == "1")*/);
+                        parameter.ListViewBill.ItemsSource = listHD;
+                        break;
+                    }                
+            }
+
         }
 
-        void _DetailPd(ManagerView paramater)
-        {
-            DetailBill detailProduct = new DetailBill();
-            HOADON temp = (HOADON)paramater.ListViewBill.SelectedItem;
-            detailProduct.MaHD.Text = temp.SOHD.ToString();
-            detailProduct.GIA.Text = string.Format("{0:0,0}", temp.TONGTIEN) + " VNĐ";
-            detailProduct.NGMH.Text = temp.NGMH.ToString();
-            detailProduct.MAKH.Text = temp.MAKH;
-            detailProduct.MACCH.Text = temp.MAKH;
-            detailProduct.ShowDialog();
-        }
+
     }
 }
