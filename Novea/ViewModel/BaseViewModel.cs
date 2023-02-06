e0ccf51cf78d4312ee62d8a73dd1b91f1905237e
Vũ Đức Minh <21522348@ms.uit.edu.vn>
2023-02-06 22:48:08 +0700

    test get notification when receive new order

--- Novea/ViewModel/BaseViewModel.cs
+++ Novea/ViewModel/BaseViewModel.cs
@@ -12,7 +12,16 @@ namespace Novea.ViewModel
     public class BaseViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
-
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(storage, value))
+            {
+                return false;
+            }
+            storage = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChangedEventHandler handler = PropertyChanged;
